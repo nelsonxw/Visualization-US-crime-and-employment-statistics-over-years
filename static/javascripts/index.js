ea16fbb0717aca458d95ba75cf22545bf01948e3
Nelson Wang <wx_nelson@yahoo.com>
2018-05-28 22:21:28 -0500

    added % to pie chart and fixed a few bugs

--- static/javascripts/index.js
+++ static/javascripts/index.js
@@ -15,7 +15,7 @@ var width_b = svgWidth_b - margin_b.left - margin_b.right;
 var height_b = svgHeight_b - margin_b.top - margin_b.bottom;
 
 /*create the SVG container and set the origin point of bubble chart*/
-var svg_b = d3.select("#bubble_Chart").append("svg")
+var svg_b = d3.select("#bubbleChartSection").append("svg")
   .attr("id","bubbleChart")
   .attr("width", svgWidth_b)
   .attr("height", svgHeight_b);
@@ -64,7 +64,7 @@ bubbleChartGroup.append("text")
 
 /*add year label and set it to be the first year in the data*/
 var label_b = bubbleChartGroup.append("text")
-  .attr("class", "year label")
+  .attr("class", "yearLabel")
   .attr("text-anchor", "end")
   .attr("x", parseInt(`${margin_b.left}`)+width_b/2)
   .attr("y", parseInt(`${margin_b.top}`)+height_b/8)
@@ -148,29 +148,21 @@ d3.json(routeURL, function(data) {
 		toolTip_b.hide(data);
 	});	
 /*==============================================*/
-/*	circleGroup.on("click", datum => {
-		console.log(datum);
-	});*/
 
-/*	circleGroup.on("click", function (d) {
-		console.log(label.text());
-	});*/
   circleGroup.on("click", function (d) {
 		/*dataFilter(d.stateCode);*/
 		var selectedYear = label_b.text();
 		var selectedState = d.stateName;
-		var totalCrime = Math.round(d.crimeRate);
 
 		var crimeBreakdown = [
-			{type:"Murder",value:Math.round(d.murderRate)},
-			{type:"Rape",value:Math.round(d.rapeRate)},
-			{type:"Robbery",value:Math.round(d.robberyRate)},
-			{type:"Assault",value:Math.round(d.assaultRate)}
+			{type:"Murder", value:Math.round(d.murderRate), percent:Math.round(d.murderRate) / Math.round(d.crimeRate)},
+			{type:"Rape", value:Math.round(d.rapeRate), percent:Math.round(d.rapeRate) / Math.round(d.crimeRate)},
+			{type:"Robbery", value:Math.round(d.robberyRate), percent:Math.round(d.robberyRate) / Math.round(d.crimeRate)},
+			{type:"Assault", value:Math.round(d.assaultRate), percent:Math.round(d.assaultRate) / Math.round(d.crimeRate)}
 		];
 		var sortedBreakdown = crimeBreakdown.sort(function(a,b){
 			return b.value - a.value;
 		});
-		
 		extraChart(selectedYear,selectedState,sortedBreakdown);
 	});
 
@@ -183,13 +175,14 @@ d3.json(routeURL, function(data) {
 	/*add an overlay on top of year label*/
   var box_b = label_b.node().getBBox();
   var overlay_b = bubbleChartGroup.append("rect")
-		.attr("class", "overlay_b")
+		.attr("class", "overlayBox")
 		.attr("x", box_b.x)
 		.attr("y", box_b.y)
 		.attr("width", box_b.width)
 		.attr("height", box_b.height-30)
 		.on("mouseover", enableInteraction);
-  
+		
+
   /*define a function to start transition of bubbles*/
   function animate() {
 	if (d3.select("#barChart")) {
@@ -197,15 +190,22 @@ d3.json(routeURL, function(data) {
 	}
 
 	if (d3.select("#pieChart")) {
-		d3.select("#barChart").remove();
+		d3.select("#pieChart").remove();
 	}
 
 	hideButton();
 
 	bubbleChartGroup.transition()
-	.duration(30000)
-	.ease(d3.easeLinear)
-	.tween("year",tweenYear); /*use tween method to create transition frame by frame*/
+		.duration(30000)
+		.ease(d3.easeLinear)
+		.tween("year",tweenYear); /*use tween method to create transition frame by frame*/
+
+	setTimeout(showObservation, 30500);
+
+	function showObservation() {
+		d3.selectAll("li")
+			.style("color","black")
+	}
   }
 
   /*define a function to interpolate a year (or a fraction of year) from the list of years, and it is used to create transition frames*/
@@ -286,21 +286,11 @@ d3.json(routeURL, function(data) {
   /*create a play button to start animation for demo*/
 	d3.select("#title").on("click",animate);
 
-
-
-
-
-
-
 });
 
 
   /*=======================================*/
-  /*function dataFilter(stateCode) {
-  	console.log(bubbleData.filter(d=>d.stateCode == stateCode));
-  }
 
-});*/
 	function extraChart(clickedYear, clickedState, clickedData) {
 		/*define svg container dimensions for extra bar chart and pie chart*/
 		
@@ -309,12 +299,13 @@ d3.json(routeURL, function(data) {
 			createBar();
 		} else if (document.querySelector("#pieChart")) { 
 			d3.select("#pieChart").remove()
-			createPie();
+			createBar();
 		} else {
 			createBar();
 		};	
 
 		function createBar(){
+		
 			valueCount = clickedData.map(d=>d.value).length;
 
 			var svgWidth_e = 500;
@@ -323,7 +314,7 @@ d3.json(routeURL, function(data) {
 			/*define margins around extra chart area*/
 			var margin_e = {
 			  top: 60,
-			  right: 50,
+			  right: 80,
 			  bottom: 150,
 			  left: 100
 			};
@@ -333,7 +324,7 @@ d3.json(routeURL, function(data) {
 			var height_e = svgHeight_e - margin_e.top - margin_e.bottom;
 
 			/*create the SVG container and set the origin point of extra chart*/
-			var svg_e = d3.select("#extra_Chart").append("svg")
+			var svg_e = d3.select("#extraChartSection").append("svg")
 			  .attr("id","barChart")
 			  .attr("width", svgWidth_e)
 			  .attr("height", svgHeight_e);
@@ -363,12 +354,12 @@ d3.json(routeURL, function(data) {
 
 			var extraChartGroup = svg_e.append("g")
 			  .attr("transform", `translate(${margin_e.left}, ${margin_e.top})`)
-			  .attr("class","extra chart");
+			  .attr("class","extraChart");
 
 
 			/*configure a band scale for the y axis with a padding of 0.1 (10%)*/
 			var yBandScale_e = d3.scaleBand()
-				.domain(clickedData.map(d => d.type))
+				.domain(clickedData.map(d => d.type).reverse())
 				.range([height_e,0])
 				.paddingInner(0.01);
 
@@ -391,19 +382,20 @@ d3.json(routeURL, function(data) {
 			var color = d3.scaleOrdinal()
     			.range(["#98abc5", "#7b6888", "#a05d56", "#ff8c00"]);
 
-			var bGroup = extraChartGroup.selectAll(".bar")
+			var bGroup = extraChartGroup.selectAll(".barGroup")
 			  .data(function() {
 		            return pie(clickedData);
 		        })
 			  .enter()
 			  .append("g")
-			  .attr("class", "bar");
+			  .attr("class", "barGroup");
 
 			bGroup.append("rect")
 			  .attr("width", d => xLinearScale_e(d.data.value))
 			  .attr("height", yBandScale_e.bandwidth())
 			  .attr("x", 0)
 			  .attr("y", function(data,index) {
+			  
 			    return index * (yBandScale_e.bandwidth() + 1);
 			  })
 			  .attr("rx",5)
@@ -412,10 +404,11 @@ d3.json(routeURL, function(data) {
 			  .style("fill",function(d) {
 			            return color(d.data.type);
 			        })
+			  .attr("class","bar")
 			  
 
 
-			extraChartGroup.selectAll(".bar")
+			extraChartGroup.selectAll(".barGroup")
 				.append("path")
 		        .style("fill",function(d) {
 			            return color(d.data.type);
@@ -426,8 +419,6 @@ d3.json(routeURL, function(data) {
 			function addBarAxis () {
 				extraChartGroup.append("g")
 				  .attr("class", "barYAxis")
-				  .style("font-size", "10px")
-				  .style("font-weight", "bold")
 				  .call(leftAxis_e);
 			}
 			
@@ -443,9 +434,7 @@ d3.json(routeURL, function(data) {
 				  .attr("y",function(data,index) {
 				    return index * height_e / valueCount + 5 + yBandScale_e.bandwidth() / 2;
 				  })
-				  .text(d=>d.value)
-				  .style("font-size", "10px")
-				  .style("font-weight","bold");
+				  .text(d=>d.value + " per 100K")
 			}
 			
 
@@ -453,7 +442,7 @@ d3.json(routeURL, function(data) {
 				
 
 				if (document.querySelector("#barChart")) {
-					d3.selectAll("rect").remove();
+					d3.selectAll(".bar").remove();
 					d3.selectAll(".barYAxis").remove();
 					d3.selectAll(".barValues").remove();
 					extraChartGroup.selectAll("path")
@@ -465,7 +454,7 @@ d3.json(routeURL, function(data) {
 				      
 				        var path = d3.select(this);
 				        var y0 = d.index * yBandScale_e.bandwidth();
-				        /*console.log(y0);*/
+				     
 				        return function(t) {
 				            var a = Math.cos(t * Math.PI / 2);
 				            var r = (1 + a) * height_e / Math.min(1, t + .005);
@@ -481,11 +470,56 @@ d3.json(routeURL, function(data) {
 
 				            path.attr("transform", `translate(${xx},${yy})`);
 				            path.attr("d", arc(f));
+				            path.on("mouseover",showSliceInfo);
+				            path.on("mouseout",hideSliceInfo);
+
+						    function showSliceInfo() {
+								if(document.querySelector("#pieChart")) {
+									var slice = d3.select(this)
+										.attr("stroke","#fff")
+			              				.attr("stroke-width","2px");
+									var sliceIndex = (slice._groups[0][0].__data__.index);
+									var sliceType = clickedData[sliceIndex].type;
+									var slicePercent = clickedData[sliceIndex].percent;
+									
+
+									svg_e.append("g").append("text")
+									  .attr("class", "crimeType")
+									  .attr("text-anchor", "middle")
+									  .attr("x", 260)
+									  .attr("y", 145)
+									  .text(`${sliceType}`);
+
+									svg_e.append("g").append("text")
+									  .attr("class", "crimePercent")
+									  .attr("text-anchor", "middle")
+									  .attr("x", 260)
+									  .attr("y", 165)
+									  .text(function () {
+									  	return d3.format(".1%")(`${slicePercent}`);
+									  });
+
+								}
+							}
+
+							function hideSliceInfo() {
+								if(document.querySelector("#pieChart")) {
+									d3.select(this)
+										.attr("stroke","none");
+			              			d3.select(".crimeType").remove();	
+			              			d3.select(".crimePercent").remove();	
+									
+								}
+							}
 				        };
 				    }
 
+					
+
+					
+
 				}
-				d3.select("#extra_Chart").select("svg")
+				d3.select("#extraChartSection").select("svg")
 					.attr("id","pieChart");
 			}
 
@@ -499,6 +533,9 @@ d3.json(routeURL, function(data) {
 				        .transition()
 				        .duration(500)
 				        .tween("arc", arcTween);
+				        
+
+				    
 
 				    function arcTween(d) {
 				  
@@ -529,7 +566,7 @@ d3.json(routeURL, function(data) {
 
 				    setTimeout(addBarAxis, 600);
 				    setTimeout(addBarValues, 600);
-				    d3.select("#extra_Chart").select("svg")
+				    d3.select("#extraChartSection").select("svg")
 					.attr("id","barChart");
 
 				}
@@ -537,16 +574,11 @@ d3.json(routeURL, function(data) {
 			d3.select("#barButton").on("click",toBar);
 			d3.select("#pieButton").on("click",toPie);
 
-
-
-
-
-
 		}
 
 
 
-	var selectedChart = d3.select("#extra_Chart");
+	var selectedChart = d3.select("#extraChartSection");
 	selectedChart
 		.on("mouseover", showButton)
 		.on("mouseout", hideButton);
@@ -572,3 +604,4 @@ function showButton() {
 		.classed("active",false)
 		.classed("inactive", true);
 	} 
+
